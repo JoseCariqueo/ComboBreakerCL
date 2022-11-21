8343baf48026fab5f514f673337dfd4db11c1a43
JoseCariqueo <83247611+JoseCariqueo@users.noreply.github.com>
2022-11-21 17:51:55 -0300

    recuperacion

---
+++ src/Components/Botones/Botones.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Botones.css'
+import Button from '@mui/material/Button';
+
+const Botones = () => {
+  return (
+    
+      <div id='menu'>
+        
+               <ul>
+               <Button className='botones'>Inicio</Button>
+               <Button className='botones'>Noticias</Button>
+               <Button className='botones'>Contacto</Button>
+              
+              </ul>
+          </div>
+    
+  )
+}
+
+export default Botones
---
+++ src/Components/Menu.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "../Components/Menu.css"
+import Logo from '../Components/images/CB.png'
+import Botones from './Botones/Botones'
+import video from '../Components/images/backg2.mp4'
+
+
+
+
+
+
+
+const Menu = () => {
+  return (
+    <div >
+    
+
+        <nav id='cabezera'>
+            <img src={Logo} className="logo"/> 
+        </nav>
+        <Botones/>
+      
+       
+        <h1 >Welcome!</h1>
+        
+        <video autoPlay loop muted playsInline className='back-video'>
+          <source src={video} type='video/mp4'/>
+        </video>
+        
+ 
+        </div>
+
+  )
+}
+
+export default Menu
